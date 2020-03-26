3fb34fbac0f33ee021ec9f01019a5920efe45ef6
Robvanzoelen <61021695+Robvanzoelen@users.noreply.github.com>
2020-03-26 16:55:48 +0100

    Final version LCP assesment Rob and Evert

--- Draw terms.py
+++ Draw terms.py
@@ -4,27 +4,23 @@
 import random
 
 with open("bingo_terms.txt", "r") as input_file:                            # Collect the words from the file as string
-    input = str(input_file.readlines())
+    inp = str(input_file.readlines())
 
 terms = []                                                                  # Create a list of our terms, in random order
-input_split = input.split()
+input_split = inp.split()
 for i in input_split:
     terms.append(i)
 del terms[0]
 del terms[len(terms)-1]
 random.shuffle(terms)
 
-message = ""
 for round in range(25):                                                     # Draw the words within the list, one by one when a sign is given
+    print("---------------------------------------------------------------------")
     print("You're now in round", round+1,".")
     while True:
-        answer = int(input(message + " type '1' if you are ready for the next word:"))
-        if answer > 1:
-            message = str(answer) + " is not 1, please"
-        elif answer < 1:
-            message = str(answer) + " is not 1, please"
+        answer = input(" Type any character or press 'Enter' to obtain the next word:")
+        if not len(answer) >= 0:
+            print(" Please try any other character.")
         else:
             break
-    print(terms[round])
-
-print(terms)
+    print(" The drawn word is:", terms[round])
